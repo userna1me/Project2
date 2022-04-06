9119fdaaf4b453caeee8f35b206165e7678a415b
userna1me <89864235+userna1me@users.noreply.github.com>
2022-04-06 14:46:50 -0700

    Add files via upload

--- src/package1/translator.java
+++ src/package1/translator.java
@@ -1,5 +1,11 @@
 package package1;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class translator {
 	
 	public static void main(String args[]) {
@@ -8,6 +14,27 @@ public class translator {
 		// translate it
 		// produce output file
 		
+		List<String> fileContents = readFile(args[0]); // assuming args[0] is input filename
+		
+		
+	}
+	
+	private static List<String> readFile(String filename) {
+		List<String> fileContents = new ArrayList<>();
+		try {
+			
+			File f = new File(filename);
+			Scanner s = new Scanner(f);
+			while(s.hasNextLine()) {
+				String line = s.nextLine();
+				fileContents.add(line);
+			}
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		return fileContents;
 	}
 	
 }
